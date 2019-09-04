81e50b399f1d99bad666195b915f084eecd78591
Jorge0e44 <33666874+Jorge0e44@users.noreply.github.com>
2019-09-04 16:54:32 -0300

    Create ColVeiculo.java

---
+++ implementacao/MyOficinaServicos/src/unioeste/geral/oficina/col/ColVeiculo.java
@@ -0,0 +1,20 @@
+package unioeste.geral.oficina.col;
+
+import java.util.ArrayList;
+
+import unioeste.apoio.BD.SQLConnector;
+import unioeste.geral.oficina.dao.DAOVeiculo;
+import unioeste.geral.oficina.bo.Veiculo;
+
+public class ColVeiculo {
+	public Veiculo obterVeiculoPorID(Veiculo veiculo, SQLConnector connector) throws Exception{
+		DAOVeiculo dao = new DAOVeiculo();
+		
+		return dao.obterVeiculoPorID(veiculo,connector);		
+	}
+	
+	public ArrayList<Veiculo> obterTodosVeiculos(SQLConnector connector) throws Exception{
+		DAOVeiculo dao = new DAOVeiculo();
+		return dao.obterTodosVeiculos(connector);
+	}
+}
