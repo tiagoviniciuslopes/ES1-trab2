df473834361913f23da7617e819431cfe21eb5e9
Jorge0e44 <33666874+Jorge0e44@users.noreply.github.com>
2019-09-05 16:17:56 -0300

    Update ColOrdemServico.java

--- implementacao/MyOficinaServicos/src/unioeste/geral/oficina/col/ColOrdemServico.java
+++ implementacao/MyOficinaServicos/src/unioeste/geral/oficina/col/ColOrdemServico.java
@@ -1,5 +1,23 @@
 package unioeste.geral.oficina.col;
 
+import java.util.ArrayList;
+
+import unioeste.apoio.BD.SQLConnector;
+import unioeste.geral.oficina.bo.OrdemServico;
+import unioeste.geral.oficina.dao.DAOOrdemServico;
+
 public class ColOrdemServico {
-		
+	public OrdemServico ObterOrdemServicoId(OrdemServico ordemservico, SQLConnector connector) throws Exception{
+
+	DAOOrdemServico dao = new DAOOrdemServico();
+	return dao.ObterOrdemServicoId(ordemservico, connector);
+	
+	}
+	
+	public ArrayList<OrdemServico> obterTodosOrdemServicoId(SQLConnector connector) throws Exception{
+
+		DAOOrdemServico dao = new DAOOrdemServico();
+		return dao.obterTodosOrdemServicoId(connector);
+	
+	}
 }
