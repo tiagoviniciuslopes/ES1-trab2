86e6d7420f702effe9914b76cca9aae1107eb2c3
tiagoviniciuslopes <tiago.foz@hotmail.com>
2019-09-05 21:12:03 -0300

    adicionando busca externa de endereco

--- implementacao/MyEnderecoServicos/src/unioeste/geral/endereco/manager/UCEnderecoGeralServicos.java
+++ implementacao/MyEnderecoServicos/src/unioeste/geral/endereco/manager/UCEnderecoGeralServicos.java
@@ -1,8 +1,15 @@
 package unioeste.geral.endereco.manager;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
+import org.json.JSONObject;
+
 import unioeste.apoio.BD.SQLConnector;
+import unioeste.geral.endereco.bo.Bairro;
 import unioeste.geral.endereco.bo.Cidade;
 import unioeste.geral.endereco.bo.Endereco;
 import unioeste.geral.endereco.bo.Logradouro;
@@ -50,12 +57,48 @@ public class UCEnderecoGeralServicos {
 		return enderecos;
 	}
 	
-	public Endereco obterEnderecoExterno(String site) {
+	public Endereco obterEnderecoExterno(String cep) throws Exception{
 		
-		//pesquisa em um site
+		JSONObject obj = new JSONObject(getHttpGET("http://viacep.com.br/ws/" + cep + "/json/"));
 		
-		return new Endereco();
+		Endereco e = new Endereco();
+		e.setCep(obj.getString("cep"));
+		Bairro b = new Bairro();
+		b.setNome(obj.getString("bairro"));
+		Logradouro l = new Logradouro();
+		l.setNome(obj.getString("logradouro"));
+		Cidade c = new Cidade();
+		c.setNome(obj.getString("localidade"));
+		UF uf = new UF();
+		uf.setNome(obj.getString("uf"));
+		c.setUf(uf);
+		e.setBairro(b);
+		e.setCidade(c);
+		e.setLogradouro(l);
+		
+		
+		return e;
 	}
+	
+	public final String getHttpGET(String urlToRead) throws Exception{
+        StringBuilder result = new StringBuilder();
+
+      
+            URL url = new URL(urlToRead);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            
+    
+        
+        return result.toString();
+    }
+
 	
 	public Cidade obterCidadePorId(Cidade cidade,  SQLConnector connector) throws Exception{
 		ColCidade colCidade = new ColCidade();
